43434610a6c0fe284d3b8f6fe7bab47f13cd645f
Sandor Suszta <sasha.shusta@gmail.com>
2023-08-20 19:31:24 +0100

    Add icons

--- Mortgage Calculator/Extensions/priceFormatter+Formatter.swift
+++ Mortgage Calculator/Extensions/priceFormatter+Formatter.swift
@@ -4,6 +4,7 @@ extension Formatter {
     static let MCPriceFormatter: Formatter = {
         let formatter = NumberFormatter()
         formatter.maximumFractionDigits = 0
+        formatter.maximumIntegerDigits = 7
         formatter.numberStyle = .currency
         formatter.currencyCode = "GBP"
         return formatter
